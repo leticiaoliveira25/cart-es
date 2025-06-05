8af9fda9f1f07487277abca3b1640bb16d794eb5
leticiaoliveira25 <oliveira.leticia25@escola.pr.gov.br>
2025-06-05 10:31:36 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Sociologia',
+    'Qual o nome do sociólogo que defende a luta de classes para moldar a sociologia?',
+    'Karl Marx'
+)
+
+criaCartao(
+    'Geografia',
+    'O que o El niño causa na águas do Pacífico?',
+    'Causa o aquecimento da águas do Pacífico'
+)
+
+criaCartao(
+    'BIOLOGIA',
+    'Qual é a célula da flor?',
+    'A célula eucrionte vegetal'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz unha em inglês?',
+    'Unha em inglês é nail'
+)
